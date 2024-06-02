74ab5ccb9a401323a74e4330c2355961d86abee3
DoanCongQui <congt4644@gmail.com>
2024-06-02 12:25:40 +0700

    Update Test 1

--- NhapMon/300_Bai_Code/Test1.c
+++ NhapMon/300_Bai_Code/Test1.c
@@ -2,16 +2,16 @@
 #include <math.h>
 int main (){
     double S, R, V;
-
+    double pi = 3.141593;
     printf("Nhap vao diem tich hinh cau: ");
     scanf("%lg", &S);
      
     // Tin ban kinh thong qua dien tich S
-    R = sqrt(S/(4*3.14));
+    R = sqrt(S/(4*pi));
 
     // Tinh the tich hinh cau 
-    V = (4/3)*3.14*R*R*R;
+    V = (4/3)*pi*R*R*R;
 
     // In ra gia the tich hinh tron
-    printf("The tich hinh cau la: %g", V);
+    printf("The tich hinh cau la: %g\n", V);
 }
